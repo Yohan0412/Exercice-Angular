a768ae7fc8fdb8c23ebc83c1a592476a12414ce7
Yohan0412 <Yohan.rosano@hotmail.fr>
2024-11-23 10:13:46 +0100

    Ajout Home page + css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { Exercice1Component } from './exercice1/exercice1.component';
 import { MonComposantComponent } from './mon-composant/mon-composant.component';
 import { MyFormComponent } from './my-form/my-form.component';
+import { MyHomeComponent } from './my-home/my-home.component';
 
 export const routes: Routes = [
+    
+    {path:'',component: MyHomeComponent },
     {path:'Exercice 1', component: Exercice1Component },
     {path:'Exercice 2', component: MonComposantComponent  },
     {path: 'Formu', component: MyFormComponent}
---
+++ src/app/my-home/my-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-home.component.html',
+  styleUrl: './my-home.component.css'
+})
+export class MyHomeComponent {
+ image : string = 'https://www.openlogic.com/sites/default/files/image/2023-01/image-blog-openlogic-what-is-angularjs-1.png';
+}
