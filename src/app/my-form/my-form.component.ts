f1b4461a2a996911d9f06244b6a056f715ab2e5b
Yohan0412 <Yohan.rosano@hotmail.fr>
2024-11-23 11:23:05 +0100

    Modification Erreur exercice 2 + debut exercice formulaire

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Order } from '../Models/order.model';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-my-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './my-form.component.html',
   styleUrl: './my-form.component.css'
 })
 export class MyFormComponent {
+
+  newOrder: Order = {
+    title: "",
+    quantity: 0,
+    date: new Date(),
+    contact: ""
+  };
+
+  onSubmit(): void {
+    // Formulaire envoyé
+    console.log(this.newOrder);
+  }
+
 
 }
