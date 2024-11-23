94ea72f2c1e8b8446d30bf6434efc7d24291d430
Yohan0412 <Yohan.rosano@hotmail.fr>
2024-11-23 09:54:52 +0100

    Ajout CSS Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Exercice1Component } from './exercice1/exercice1.component';
 import { MonComposantComponent } from './mon-composant/mon-composant.component';
+import { MyFormComponent } from './my-form/my-form.component';
 
 export const routes: Routes = [
     {path:'Exercice 1', component: Exercice1Component },
     {path:'Exercice 2', component: MonComposantComponent  },
+    {path: 'Formu', component: MyFormComponent}
      
 ];
